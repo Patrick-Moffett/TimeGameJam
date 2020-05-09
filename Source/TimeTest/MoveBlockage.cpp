81e3c11b5f38523c92df7be66d9a3f0f3891c6ac
LilJakey <jacobhopper.mail@gmail.com>
2020-05-08 19:53:43 -0500

    Completed MoveBlockage, temporary jumping area

--- Source/TimeTest/MoveBlockage.cpp
+++ Source/TimeTest/MoveBlockage.cpp
@@ -2,33 +2,112 @@
 
 
 #include "MoveBlockage.h"
+#include "Components/AudioComponent.h"
+#include "Components/PrimitiveComponent.h"
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UMoveBlockage::UMoveBlockage()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UMoveBlockage::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
-}
+	InitialLocation = GetOwner()->GetActorLocation();
+	CurrentLocation = InitialLocation;
 
+	CheckForPressurePlate();
+	FindAudioComponent();
+}
 
 // Called every frame
 void UMoveBlockage::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (TotalMassOfActors() >= MassToOpenDoor)
+	{
+		OpenBlockage(DeltaTime);
+		BlockageLastMoved = GetWorld()->GetTimeSeconds();
+	}
+	else
+	{
+		if (GetWorld()->GetTimeSeconds() - BlockageLastMoved > BlockageCloseDelay)
+		{
+			CloseBlockage(DeltaTime);
+		}
+	}
 }
 
+void UMoveBlockage::OpenBlockage(float DeltaTime)
+{
+	CurrentLocation = GetOwner()->GetActorLocation();
+	FVector BlockageMovement = { 0.f, 0.f, 0.f };
+	BlockageMovement = FMath::VInterpTo(CurrentLocation,(InitialLocation + TargetMovement), DeltaTime, MoveSpeed);
+	GetOwner()->SetActorLocation(BlockageMovement);
+
+	if (!AudioComponent) { return; }
+	if (!HasOpenSoundPlayed)
+	{
+		AudioComponent->Play();
+		HasOpenSoundPlayed = true;
+		HasCloseSoundPlayed = false;
+	}
+}
+
+void UMoveBlockage::CloseBlockage(float DeltaTime)
+{
+	CurrentLocation = GetOwner()->GetActorLocation();
+	FVector BlockageMovement = { 0.f, 0.f, 0.f };
+	BlockageMovement = FMath::VInterpTo(CurrentLocation, InitialLocation, DeltaTime, MoveSpeed);
+	GetOwner()->SetActorLocation(BlockageMovement);
+
+	if (!AudioComponent) { return; }
+	if (!HasCloseSoundPlayed)
+	{
+		AudioComponent->Play();
+		HasCloseSoundPlayed = true;
+		HasOpenSoundPlayed = false;
+	}
+}
+
+float UMoveBlockage::TotalMassOfActors() const
+{
+	float TotalMass = 0.f;
+	TArray<AActor*> OverlappingActors;
+	if (!PressurePlate) { return TotalMass; }
+	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
+
+	for (AActor* Actor : OverlappingActors)
+	{
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+	}
+
+	return TotalMass;
+}
+
+void UMoveBlockage::FindAudioComponent()
+{
+	AudioComponent = GetOwner()->FindComponentByClass<UAudioComponent>();
+
+	if (!AudioComponent)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s Missing Audio Component!"), *GetOwner()->GetName());
+	}
+}
+
+void UMoveBlockage::CheckForPressurePlate() const
+{
+	if (!PressurePlate)
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s has the open door componet on it, but no pressureplate set!"), *GetOwner()->GetName());
+	}
+}
--- Source/TimeTest/MoveBlockage.h
+++ Source/TimeTest/MoveBlockage.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "MoveBlockage.generated.h"
 
 
@@ -12,17 +13,47 @@ class TIMETEST_API UMoveBlockage : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	UMoveBlockage();
-
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
+	// Sets default values for this component's properties
+	UMoveBlockage();
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	void OpenBlockage(float DeltaTime);
+	void CloseBlockage(float DeltaTime);
+	float TotalMassOfActors() const;
+	void FindAudioComponent();
+	void CheckForPressurePlate() const;
 
-		
+private:
+
+	// move blockage
+	FVector InitialLocation;
+	FVector CurrentLocation;
+
+	UPROPERTY(EditAnywhere)
+		FVector TargetMovement = { 0.f, 0.f, 0.f };
+
+	UPROPERTY(EditAnywhere)
+		float MoveSpeed = 0.4f;
+
+	UPROPERTY(EditAnywhere)
+		float BlockageCloseDelay = 0.3f;
+
+	float BlockageLastMoved = 0.f;
+
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* PressurePlate = nullptr;
+
+	UPROPERTY(EditAnywhere)
+		float MassToOpenDoor = 50.f;
+
+	UPROPERTY()
+		UAudioComponent* AudioComponent = nullptr;
+
+	// tracks wether sound has been played
+	bool HasOpenSoundPlayed = false;
+	bool HasCloseSoundPlayed = true;
 };
